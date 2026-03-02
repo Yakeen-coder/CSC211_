e6935838d3063f50e13b7a6aa77cfd7c1da4d8e7
Yakeen-coder <4482051@myuwc.ac.za>
2026-03-02 15:16:22 +0200

    Testing

--- openHash.java
+++ openHash.java
@@ -1,5 +1,34 @@
+import java.util.*;
+import java.nio.file.*;
 
 
 public class openHash {
-    
-}
+    private int m; // Size of the table
+    private int count; // Number of elements currently in the table
+    private Node[] table;
+
+    // The Node class handles the "Chaining" (Open Hashing)
+    private static class Node {
+        String key;
+        String value;
+        Node next;
+
+        Node(String k, String v) {
+            this.key = k;
+            this.value = v;
+        }
+    }
+
+    public openHash(int capacity) {
+        this.m = capacity;
+        this.table = new Node[m];
+        this.count = 0;
+    }
+
+    //Scatter function: Uniform distribution
+
+    public int hash(String key) {
+        return Math.abs(key.hashCode()) % m;
+    }
+
+
