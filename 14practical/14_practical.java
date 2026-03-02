0b0fa00784104a497e73911836d9a28ed3c1d120
Yakeen-coder <4482051@myuwc.ac.za>
2026-03-02 14:14:09 +0200

    Start Hashing Project

---
+++ 14practical/14_practical.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.nio.file.*;
+import java.io.*;
+
+public class 14_practical{
+    public static void main(String[] args){
+        Path path = Paths.get("")
+    }
+}
